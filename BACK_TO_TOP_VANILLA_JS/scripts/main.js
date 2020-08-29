683ed63c4fab0d3cddca459673b660d2976818b7
Piotr <piotr.slobinski@gmail.com>
2020-08-29 13:31:04 +0200

    back to top button in vanilla js

---
+++ BACK_TO_TOP_VANILLA_JS/scripts/main.js
@@ -0,0 +1,22 @@
+// alert('działa')
+
+const TOP = document.getElementById('TOP');
+
+let intervalId = 0; // Needed to cancel the scrolling when we're at the top of the page
+// const $scrollButton = document.querySelector('.scroll'); // Reference to our scroll button
+
+function scrollStep() {
+    // Check if we're at the top already. If so, stop scrolling by clearing the interval
+    if (window.pageYOffset === 0) {
+        clearInterval(intervalId);
+    }
+    window.scroll(0, window.pageYOffset - 50);
+}
+
+function scrollToTop() {
+    // Call the function scrollStep() every 16.66 millisecons
+    intervalId = setInterval(scrollStep, 16.66);
+}
+
+// When the DOM is loaded, this click handler is added to our scroll button
+TOP.addEventListener('click', scrollToTop);
